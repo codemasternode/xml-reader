22628925be92e627976ddf5dcca0d8d7509e124c
Marcin <marcinwarzybok@outlook.com>
2020-09-22 15:03:35 +0200

    modernize

--- import.js
+++ import.js
@@ -8,16 +8,9 @@ MongoClient.connect(url, { useUnifiedTopology: true }, async (err, db) => {
   if (err) throw err;
   const dbo = db.db("reservations");
 
-  const [offers, places] = await Promise.all([getOffers(), getPlaces()]);
-
+  const offers = await getOffers();
   dbo.collection("offers").insertMany(offers, function (err, res) {
     if (err) throw err;
-    console.log("Saved offers");
-    dbo.collection("places").insertMany(places, function (err, res) {
-      if (err) throw err;
-      console.log("Saved places");
-      db.close();
-    });
   });
 });
 
@@ -29,94 +22,58 @@ const getOffers = () => {
         console.error(err);
       }
       let fileContent = data.content;
-      fileContent = fileContent.slice(
-        fileContent.search("<offers>") + 8,
-        fileContent.search("</offers>")
-      );
-      fileContent = fileContent.split("</offer>");
-      fileContent.splice(fileContent.length - 1);
-      let offersToSave = [];
-      for (let i = 0; i < fileContent.length; i++) {
-        let offer = fileContent[i];
-        //console.log(offer);
-
-        const idObject = getValueFromAttribute(offer, "idObject");
-        offer = offer.replace("idObject", "");
-        const id = getValueFromAttribute(offer, "id");
-        const readyFrom = new Date(getValueFromTag(offer, "readyFrom"));
-        const readyTo = new Date(getValueFromTag(offer, "readyTo"));
-        const name = getValueFromTag(offer, "name");
-
-        const reservations = offer
-          .slice(
-            offer.search("<reservations>") + 14,
-            offer.search("</reservations>")
-          )
-          .split("/>");
-        reservations.splice(reservations.length - 1);
-        let convertedReservations = [];
-        for (let k = 0; k < reservations.length; k++) {
-          const from = new Date(getValueFromAttribute(reservations[k], "from"));
-          const to = new Date(getValueFromAttribute(reservations[k], "to"));
-          convertedReservations.push({
-            from,
-            to,
-          });
+      fileContent = fileContent.replace(/(\r\n|\n|\r)/gm, "");
+      const offersToSave = [];
+      const offers = fileContent.match(/<offer[^>]*>(.+?)<\/offer>/g);
+      for (let i = 0; i < offers.length - 1; i++) {
+        const name = offers[i]
+          .match(/<name>(.*?)<\/name>/g)[0]
+          .replace(/<\/?name>/g, "");
+        const readyFrom = new Date(
+          offers[i]
+            .match(/<readyFrom>(.*?)<\/readyFrom>/g)[0]
+            .replace(/<\/?readyFrom>/g, "")
+        );
+        const readyTo = new Date(
+          offers[i]
+            .match(/<readyTo>(.*?)<\/readyTo>/g)[0]
+            .replace(/<\/?readyTo>/g, "")
+        );
+        var regex = new RegExp(
+          "[\\s\\r\\t\\n]*([a-z0-9\\-_]+)[\\s\\r\\t\\n]*=[\\s\\r\\t\\n]*(['\"])((?:\\\\\\2|(?!\\2).)*)\\2",
+          "ig"
+        );
+        var attributes = {};
+        while ((match = regex.exec(offers[i]))) {
+          attributes[match[1]] = match[3];
+        }
+        const idObject = attributes.idObject;
+        const id = attributes.id;
+        const reservations = offers[i].match(
+          /<reservation[^>]>(.*?)<\/reservation>/g
+        );
+        const reservationsS = [];
+        let attribs = {};
+        while ((match = regex.exec(offers[i]))) {
+          if (match[1] !== "id" && match[1] !== "idObject") {
+            if (Object.keys(attribs).length === 2) {
+              reservationsS.push({ ...attribs });
+              attribs = {};
+            } else {
+              attribs[match[1]] = new Date(match[3]);
+            }
+          }
         }
         offersToSave.push({
           id,
-          name,
           idObject,
+          name,
           readyFrom,
           readyTo,
-          reservations: convertedReservations,
+          reservations: reservationsS,
         });
       }
       resolve(offersToSave);
     });
   });
 };
-
-const getPlaces = () => {
-  const placesFile = path.join(__dirname, "./places.xml");
-  return new Promise((resolve, reject) => {
-    xmlReader.readXML(fs.readFileSync(placesFile), function (err, data) {
-      if (err) {
-        console.error(err);
-      }
-      let fileContent = data.content;
-      fileContent = fileContent.slice(
-        fileContent.search("<places>") + 8,
-        fileContent.search("</places>")
-      );
-      fileContent = fileContent.split("</place>");
-      fileContent.splice(fileContent.length - 1);
-      let placesToSave = [];
-      for (let i = 0; i < fileContent.length; i++) {
-        let place = fileContent[i];
-        //console.log(offer);
-        const id = getValueFromAttribute(place, "id");
-        const name = place.slice(place.search(">") + 1);
-        placesToSave.push({ id, name });
-      }
-      resolve(placesToSave);
-    });
-  });
-};
-
-function getValueFromAttribute(str, attr) {
-  let startPositionCut = str.slice(str.search(attr) + attr.length);
-  const startPosition = startPositionCut.search('"');
-  startPositionCut = startPositionCut.slice(startPositionCut.search('"') + 1);
-  const value = startPositionCut.slice(
-    startPosition - 1,
-    startPositionCut.search('"')
-  );
-  return value;
-}
-
-function getValueFromTag(str, tag) {
-  const startValuePosition = str.search(tag) + tag.length + 1;
-  const endValuePosition = str.search(`</${tag}`);
-  return str.slice(startValuePosition, endValuePosition);
-}
