73e5080b9f8f10f2439d2e5278040e8496629e1b
Marcin <marcinwarzybok@outlook.com>
2020-09-22 15:25:59 +0200

    Add some corrects

--- import.js
+++ import.js
@@ -1,30 +1,21 @@
-const fs = require("fs"),
-  path = require("path"),
-  xmlReader = require("read-xml"),
-  MongoClient = require("mongodb").MongoClient,
-  url = "mongodb://localhost:27017/";
+const fs = require("fs");
+const path = require("path");
+const reader = require("read-xml");
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, { useUnifiedTopology: true }, async (err, db) => {
-  if (err) throw err;
-  const dbo = db.db("reservations");
-
-  const offers = await getOffers();
-  dbo.collection("offers").insertMany(offers, function (err, res) {
-    if (err) throw err;
-  });
-});
-
-const getOffers = () => {
-  const offersFile = path.join(__dirname, "./offers.xml");
+function getOffers() {
+  const fileOffers = path.join(__dirname, "./offers.xml");
   return new Promise((resolve, reject) => {
-    xmlReader.readXML(fs.readFileSync(offersFile), function (err, data) {
+    reader.readXML(fs.readFileSync(fileOffers), function (err, data) {
       if (err) {
         console.error(err);
+        reject();
       }
-      let fileContent = data.content;
-      fileContent = fileContent.replace(/(\r\n|\n|\r)/gm, "");
+      let content = data.content;
+      content = content.replace(/(\r\n|\n|\r)/gm, "");
       const offersToSave = [];
-      const offers = fileContent.match(/<offer[^>]*>(.+?)<\/offer>/g);
+      const offers = content.match(/<offer[^>]*>(.+?)<\/offer>/g);
       for (let i = 0; i < offers.length - 1; i++) {
         const name = offers[i]
           .match(/<name>(.*?)<\/name>/g)[0]
@@ -39,22 +30,19 @@ const getOffers = () => {
             .match(/<readyTo>(.*?)<\/readyTo>/g)[0]
             .replace(/<\/?readyTo>/g, "")
         );
-        var regex = new RegExp(
+        var regexForAttributes = new RegExp(
           "[\\s\\r\\t\\n]*([a-z0-9\\-_]+)[\\s\\r\\t\\n]*=[\\s\\r\\t\\n]*(['\"])((?:\\\\\\2|(?!\\2).)*)\\2",
           "ig"
         );
         var attributes = {};
-        while ((match = regex.exec(offers[i]))) {
+        while ((match = regexForAttributes.exec(offers[i]))) {
           attributes[match[1]] = match[3];
         }
         const idObject = attributes.idObject;
         const id = attributes.id;
-        const reservations = offers[i].match(
-          /<reservation[^>]>(.*?)<\/reservation>/g
-        );
         const reservationsS = [];
         let attribs = {};
-        while ((match = regex.exec(offers[i]))) {
+        while ((match = regexForAttributes.exec(offers[i]))) {
           if (match[1] !== "id" && match[1] !== "idObject") {
             if (Object.keys(attribs).length === 2) {
               reservationsS.push({ ...attribs });
@@ -76,4 +64,14 @@ const getOffers = () => {
       resolve(offersToSave);
     });
   });
-};
+}
+
+MongoClient.connect(url, { useUnifiedTopology: true }, async (err, db) => {
+  if (err) throw err;
+  const dbo = db.db("database");
+  const offers = await getOffers();
+  dbo.collection("offers").insertMany(offers, function (err, res) {
+    if (err) throw err;
+    db.close();
+  });
+});
--- query.js
+++ query.js
@@ -2,134 +2,128 @@ const MongoClient = require("mongodb").MongoClient,
   url = "mongodb://localhost:27017/";
 
 if (!process.argv[2] || !process.argv[3] || !process.argv[4]) {
-  throw new Error(
-    "Incorrect format!!! Expect: npm run dev [dataOd] [dataDo] [liczbaDni]"
-  );
+  throw new Error("[dataOd] [dataDo] [liczbaDni]");
 }
 
 const dateFrom = new Date(process.argv[2]);
 const dateTo = new Date(process.argv[3]);
-const numberOfDays = Number(process.argv[4]);
+const daysToSearch = Number(process.argv[4]);
 
-if (getNumberOfDays(dateTo - dateFrom) < numberOfDays - 1) {
+if (Math.ceil((dateTo - dateFrom) / 86400400) < daysToSearch - 1) {
   throw new Error(
-    "Number of days is smaller than difference between dateFrom and dateTo"
+    "Liczba dni wyszukiwanych jest mniejsza niż różnica pomiędzy jedną a drugą dataą"
   );
 }
 
 MongoClient.connect(url, { useUnifiedTopology: true }, async (err, db) => {
   if (err) throw err;
-  const dbo = db.db("reservations");
-
+  const dbo = db.db("database");
   dbo
     .collection("offers")
     .find({})
     .toArray(function (err, offers) {
       const selectedOffers = [];
-      for (let i = 0; i < offers.length; i++) {
-        if (offers[i].readyFrom > dateTo || offers[i].readyTo < dateFrom) {
-          continue;
+      offers.forEach((offer) => {
+        if (offer.readyFrom > dateTo || offer.readyTo < dateFrom) {
+          return;
         }
 
-        const avaiableDays = [];
+        const days = [];
         for (
-          var d = new Date(offers[i].readyFrom);
-          d <= new Date(offers[i].readyTo);
+          var d = new Date(offer.readyFrom);
+          d <= new Date(offer.readyTo);
           d.setDate(d.getDate() + 1)
         ) {
-          avaiableDays.push(new Date(d));
+          days.push(new Date(d));
         }
 
-        offers[i].reservations.sort((a, b) => a.from - b.from);
-        for (let k = 0; k < offers[i].reservations.length; k++) {
-          const reservation = offers[i].reservations[k];
-          for (let m = 0; m < avaiableDays.length; m++) {
-            if (
-              avaiableDays[m] >= reservation.from &&
-              avaiableDays[m] <= reservation.to
-            ) {
-              avaiableDays[m] = null;
+        offer.reservations.sort((a, b) => a.from - b.from);
+        offer.reservations.forEach((reservation) => {
+          days.forEach((day) => {
+            if (day >= reservation.from && day <= reservation.to) {
+              day = null;
             }
+          });
+        });
+
+        days.map((day) => {
+          if (day < dateFrom || day > dateTo) {
+            day = null;
           }
+        });
+
+        let count = 1;
+        let startIndex = 0;
+        while (days[startIndex] === null && days[startIndex] !== undefined) {
+          startIndex++;
         }
 
-        for (let k = 0; k < avaiableDays.length; k++) {
-          if (avaiableDays[k] < dateFrom || avaiableDays[k] > dateTo) {
-            avaiableDays[k] = null;
-          }
-        }
-
-        let countDays = 1;
-        let start = 0;
-        while (
-          avaiableDays[start] === null &&
-          avaiableDays[start] !== undefined
-        ) {
-          start++;
-        }
-
-        loop: for (let h = start + 1; h < avaiableDays.length; h++) {
-          if (countDays === numberOfDays) {
-            const to = new Date(avaiableDays[start]);
-            to.setDate(to.getDate() + numberOfDays - 1);
+        loop: for (let h = startIndex + 1; h < days.length; h++) {
+          if (count === daysToSearch) {
+            const to = new Date(days[startIndex]);
+            to.setDate(to.getDate() + daysToSearch - 1);
             selectedOffers.push({
-              id: offers[i].id,
-              idObject: offers[i].idObject,
-              from: avaiableDays[start],
+              id: offer.id,
+              idObject: offer.idObject,
+              from: days[startIndex],
               to: to,
             });
-            countDays = 1;
-            start++;
+            count = 1;
+            startIndex++;
             while (
-              avaiableDays[start] === null &&
-              avaiableDays[start] !== undefined
+              days[startIndex] === null &&
+              days[startIndex] !== undefined
             ) {
-              start++;
+              startIndex++;
             }
-            h = start;
-            if (avaiableDays[start] === undefined) {
+            h = startIndex;
+            if (days[startIndex] === undefined) {
               break loop;
             }
-          } else if (avaiableDays[h] === null) {
-            countDays = 0;
-            start = h;
+          } else if (days[h] === null) {
+            count = 0;
+            startIndex = h;
             while (
-              avaiableDays[start] === null &&
-              avaiableDays[start] !== undefined
+              days[startIndex] === null &&
+              days[startIndex] !== undefined
             ) {
-              start++;
+              startIndex++;
             }
-          } else if (avaiableDays[h] !== null) {
-            countDays++;
+          } else if (days[h] !== null) {
+            count++;
           }
         }
 
-        if (countDays === numberOfDays) {
-          const to = new Date(avaiableDays[start]);
-          to.setDate(to.getDate() + numberOfDays - 1);
+        if (count === daysToSearch) {
+          const to = new Date(days[startIndex]);
+          to.setDate(to.getDate() + daysToSearch - 1);
           selectedOffers.push({
-            id: offers[i].id,
-            idObject: offers[i].idObject,
-            from: avaiableDays[start],
-            to
+            id: offer.id,
+            idObject: offer.idObject,
+            from: days[startIndex],
+            to,
           });
         }
-      }
-      for (let i = 0; i < selectedOffers.length; i++) {
+      });
+      selectedOffers.forEach((offer) => {
         console.log(
-          `${selectedOffers[i].idObject} ${selectedOffers[i].id} ${formatDate(
-            selectedOffers[i].from
-          )} ${formatDate(selectedOffers[i].to)}`
+          `${offer.idObject} ${offer.id} ${dateToYYYYMMDD(
+            offer.from
+          )} ${dateToYYYYMMDD(offer.to)}`
         );
-      }
+      });
       db.close();
     });
 });
 
-function getNumberOfDays(miliseconds) {
-  return Math.ceil(miliseconds / 86400400);
-}
+function dateToYYYYMMDD(date) {
+  var d = new Date(date),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
 
-function formatDate(date) {
-  return `${date.getFullYear()}-${date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1}-${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}`;
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [year, month, day].join("-");
 }
